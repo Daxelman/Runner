247e1fc26467b5570187044d7744eba743a7b1a9
Mclean Oshiokpekhai <mxo7542@rit.edu>
2015-01-15 13:01:49 -0600

    Initial Push

---
+++ New Unity Project/Assets/Scripts/CameraRunnerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRunnerScript : MonoBehaviour {
+
+
+    public Transform player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float pPos = player.position.x;
+        float cPos = pPos + 6;
+        if(this.transform != null)
+             transform.position = new Vector3(cPos, 0, -10);
+	}
+}
---
+++ New Unity Project/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+
+    public GameObject[] obj;
+    public float spawnmin = 1f;
+    public float spawnmax = 2f;
+	// Use this for initialization
+	void Start () {
+        spawn();
+	}
+	
+	void spawn()
+    {
+        Instantiate(obj[Random.Range(0,obj.GetLength(0))],transform.position,Quaternion.identity);
+        Invoke("spawn",Random.Range(spawnmin,spawnmax));
+    }
+}
